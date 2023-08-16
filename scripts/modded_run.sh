aed528951889b7e4f60d5c340f502a0e8a522865
Rana Waqas Haider <32241950+rvvh472@users.noreply.github.com>
2023-08-16 15:13:46 +0500

    Update modded_run.sh
    Updated Modded_run.sh to support 2306

--- scripts/modded_run.sh
+++ scripts/modded_run.sh
@@ -59,8 +59,12 @@ declare -A ROOT_SOL_MAP=(["magisk"]="magisk" ["none"]="none")
 #declare -A RELEASE_TYPE_MAP=(["retail"]="retail" ["release preview"]="RP" ["insider slow"]="WIS" ["insider fast"]="WIF")
 COMMAND_LINE=(--arch "${ARCH_MAP[$ARCH]}" --release-type "${RELEASE_TYPE_MAP[$RELEASE_TYPE]}" --magisk-ver "${MAGISK_VER_MAP[$MAGISK_VER]}" --gapps-brand "${GAPPS_BRAND_MAP[$GAPPS_BRAND]}" --gapps-variant "${GAPPS_VARIANT_MAP[$GAPPS_VARIANT]}" "${REMOVE_AMAZON_MAP[$REMOVE_AMAZON]}" --root-sol "${ROOT_SOL_MAP[$ROOT_SOL]}" "$COMPRESS_OUTPUT" "$OFFLINE" "$DEBUG" "$CUSTOM_MAGISK" --compress-format "$COMPRESS_FORMAT")
 echo "COMMAND_LINE=${COMMAND_LINE[*]}"
-chmod +x ./build.sh				   
-./build.sh "${COMMAND_LINE[@]}"
+# if 2306 Error is fixed
+#chmod +x ./build.sh				   
+#./build.sh "${COMMAND_LINE[@]}"
+# With trick to fix 2306
+chmod +x ./build_modded.sh				   
+./build_modded.sh "${COMMAND_LINE[@]}"
 # Magisk Always True Command Line
 #M_COMMAND_LINE=(--arch "${ARCH_MAP[$ARCH]}" --release-type "${RELEASE_TYPE_MAP[$RELEASE_TYPE]}" --magisk-ver "${MAGISK_VER_MAP[$MAGISK_VER]}" --gapps-brand "${GAPPS_BRAND_MAP[$GAPPS_BRAND]}" --gapps-variant "${GAPPS_VARIANT_MAP[$GAPPS_VARIANT]}" "${REMOVE_AMAZON_MAP[$REMOVE_AMAZON]}" --root-sol "$MAGISK_TRUE" "$COMPRESS_OUTPUT" "$OFFLINE" "$DEBUG" "$CUSTOM_MAGISK" --compress-format "$COMPRESS_FORMAT")
 #echo "M_COMMAND_LINE=${M_COMMAND_LINE[*]}"
